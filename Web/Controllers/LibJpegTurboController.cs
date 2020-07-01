e084ee643330c5c22c3822c26e9d0cc4a772869d
Stephen O'Shannessy <soshannessy@nrl.com.au>
2020-07-01 16:13:01 +1000

    Initial commit

---
+++ Web/Controllers/LibJpegTurboController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TurboJpegWrapper;
+
+namespace Web.Controllers
+{
+    public class LibJpegTurboController : Controller
+    {
+        public ActionResult Compress(string path)
+        {
+            using (var bmp = new Bitmap(path))
+            {
+                using (var tjc = new TJCompressor())
+                {
+                    // (TJFlags)16384 = Progressive Image
+                    var compressed = tjc.Compress(bmp, TJSubsamplingOption.Chrominance420, 85, (TJFlags)16384);
+                    var outPath = Path.Combine(Path.GetDirectoryName(path), $"compressed-", Path.GetFileName(path));
+                    System.IO.File.WriteAllBytes(outPath, compressed);
+                }
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Web/Controllers/MozJpegController.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Drawing;
+using System.Net;
+using System.Web.Mvc;
+using MozJpegSharp;
+
+namespace Web.Controllers
+{
+    public class MozJpegController : Controller
+    {
+        public ActionResult Compress(string path)
+        {
+            using (var bmp = new Bitmap(path))
+            {
+                using (var tjc = new TJCompressor())
+                {
+                    var compressed = tjc.Compress(bmp, TJSubsamplingOption.Chrominance420, 85, TJFlags.None);
+                    var outPath = Path.Combine(Path.GetDirectoryName(path), $"compressed-", Path.GetFileName(path));
+                    System.IO.File.WriteAllBytes(outPath, compressed);
+                }
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
